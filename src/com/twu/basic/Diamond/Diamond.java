94762953037dafa352d1e166efed5ba0a8ec7ba5
shuhan <2472520650@qq.com>
2018-07-20 21:18:37 +0800

    diamondFinish

--- src/com/twu/basic/Diamond/Diamond.java
+++ src/com/twu/basic/Diamond/Diamond.java
@@ -1,4 +1,80 @@
 package com.twu.basic.Diamond;
 
 public class Diamond {
+    public void dimondIsosceles(int number){
+        int temp = number;
+        for (int i = 0; i < temp; i ++){
+            for (int j = temp - i -1;j > 0;j --){
+                System.out.print(" ");
+            }
+            for (int k = 2*i;k >= 0;k --){
+                System.out.print("*");
+            }
+            for (int l = temp - i -1;l > 0;l --){
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+    public void dimondCentered(int number){
+        int temp = number;
+        for (int i = 0; i < temp; i ++){
+            for (int j = temp - i -1;j > 0;j --){
+                System.out.print(" ");
+            }
+            for (int k = 2 * i;k >= 0;k --){
+                System.out.print("*");
+            }
+            for (int l = temp - i -1;l > 0;l --){
+                System.out.print(" ");
+            }
+            System.out.print("\r\n");
+        }
+
+        for (int i = 0; i < temp -1; i ++) {
+            for (int j = i + 1; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 2 * (temp - i - 2); k >= 0; k--) {
+                System.out.print("*");
+            }
+            for (int l = i + 1; l > 0; l--) {
+                System.out.print(" ");
+            }
+            System.out.print("\r\n");
+        }
+
+    }
+//    public void dimondCenter(int number,int currentNumber,boolean flag){
+//            int temp = number;
+//            for (int j = temp - currentNumber - 1; j > 0; j--) {
+//                System.out.print(" ");
+//            }
+//            for (int k = 2 * currentNumber; k >= 0; k--) {
+//                System.out.print("*");
+//            }
+//            for (int l = temp - currentNumber - 1; l > 0; l--) {
+//                System.out.print(" ");
+//            }
+//            System.out.println("");
+//            if (flag) {
+//                if(currentNumber<temp-1) {
+//                    dimondCenter(number, ++currentNumber, flag);
+//                }
+//                else{
+//                    flag = !flag;
+//                    dimondCenter(number, --currentNumber, flag);
+//                }
+//            }
+//            if(!flag){
+//                if(currentNumber>0) {
+//                    dimondCenter(number, --currentNumber, flag);
+//                }
+//                else{
+//                    ;
+//                }
+//            }
+//    }
+
+
 }
--- test/com/twu/basic/DimondTest.java
+++ test/com/twu/basic/DimondTest.java
@@ -1,10 +1,14 @@
 package com.twu.basic;
 
+import com.twu.basic.Diamond.Diamond;
+import com.twu.basic.Triganle.Triganle;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 public class DimondTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -12,5 +16,18 @@ public class DimondTest {
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
     }
+    @Test
+    public void testDimaondIsosceles(){
+        Diamond dimond = new Diamond();
+        dimond.dimondIsosceles(3);
+        assertEquals("  *  \r\n *** \r\n*****\r\n", outContent.toString());
+    }
+    @Test
+    public void testDimaondCentered(){
+        Diamond dimond = new Diamond();
+//       System.out.print("  *  \r\n *** \r\n*****\r\n *** \r\n  *  \r\n");
 
+        dimond.dimondCentered(3);
+        assertEquals("  *  \r\n *** \r\n*****\r\n *** \r\n  *  \r\n", outContent.toString());
+    }
 }
